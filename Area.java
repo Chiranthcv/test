10bfb95cb19c9d0b50986ef7f828523c91af906d
Chiranthcv <chiranth775@gmail.com>
2025-03-18 12:27:53 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,13 @@
+java.util.Scanner;
+public class Area{
+	public static void main(String[]args){
+		Scanner sc = new Scanner(System.in);
+		double height = scanner.nextDouble();
+		double base = scanner.nextDouble();
+		double area = 0.5*height*base;
+		System.out.println("Area of the triangle"+area);
+		Scanner.close();
+	}
+}
+
+
