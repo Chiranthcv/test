4c0a37dec88875fa8dc151b8d92797fa5dfe3efb
Chiranthcv <chiranth775@gmail.com>
2025-03-18 12:32:45 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -1,13 +1,22 @@
 import java.util.Scanner;
-public class Area{
-	public static void main(String[]args){
-		Scanner sc = new Scanner(System.in);
-		double height = scanner.nextDouble();
-		double base = scanner.nextDouble();
-		double area = 0.5*height*base;
-		System.out.println("Area of the triangle"+area);
-		Scanner.close();
-	}
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the base of the triangle: ");
+        double base = scanner.nextDouble();
+
+        System.out.print("Enter the height of the triangle: ");
+        double height = scanner.nextDouble();
+
+        double area = 0.5 * base * height;
+
+        System.out.println("The area of the triangle is: " + area);
+
+        scanner.close();
+    }
 }
+
 
 
